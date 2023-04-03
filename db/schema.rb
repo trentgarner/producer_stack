0d0935d3ebf8d2c5c96ae708655510f39a073eff
Trent Garner <trentongarner.13@icloud.com>
2023-04-03 16:42:51 -0600

    add blog table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_203130) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_223921) do
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
