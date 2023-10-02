5f9296fecf9e58e111b1c1fb94c8481c835cc92e
Trent Garner <trentongarner.13@icloud.com>
2023-10-02 15:29:17 -0600

    User create and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controller: {
-    registrations: 'registrations'
-  }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
+  
 
   root "application#router"
 
